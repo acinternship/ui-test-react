508a72d7b526bd71514743bb52216c71f06256d3
Rafael Câmara Magalhães <rafaelcamaram@gmail.com>
2016-07-25 15:23:25 -0300

    Initial Files

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return <div>Teste</div>
+  }
+}
+
+//var App = React.createClass({
+//  render: function() {
+//    return <div>Diferente</div>
+//  }
+//});
+
+export default App
