8efc702039ddce2d23dac799102be9e2580bd216
Rafael Câmara Magalhães <rafaelcamaram@gmail.com>
2016-07-26 13:41:08 -0300

    Dynamically Generated Components

--- App.js
+++ App.js
@@ -1,54 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let Mixin = InnerComponent => class extends React.Component {
+class App extends React.Component {
   constructor(){
     super();
-    this.update = this.update.bind(this);
-    this.state = {val: 0}
+    this.state = {data: [ 
+      {id: 1, name: "Rafael Câmara"},
+		{id: 2, name: "Thomas Burleson"},
+      {id: 3, name: "Will Button"},
+		{id: 4, name: "Ben Clinkinbeard"},
+      {id: 5, name: "Kent Dodds"},
+		{id: 6, name: "Trevor Ewen"},
+      {id: 7, name: "Aaron Frost"},
+		{id: 8, name: "Joel Hooks"},
+      {id: 9, name: "Jafar Husain"},
+		{id: 10, name: "Tim Kindberg"},
+      {id: 11, name: "John Lindquist"},
+      {id: 12, name: "Joe Maddalone"},
+      {id: 13, name: "Tyler McGinnis"},
+		{id: 14, name: "Scott Moss"},
+      {id: 15, name: "Robert Penner"},
+		{id: 16, name: "Keith Peters"},
+      {id: 17, name: "Lukas Ruebbelke"},
+      {id: 18, name: "Brett Shollenberger"}
+    ]} // Like DB
   }
-  update(){
-    this.setState({val: this.state.val + 1})
-  }
-  componentWillMount(){
-    console.log('will mount')
-  }
+	
   render(){
-    return <InnerComponent
-      update={this.update}
-      {...this.state}
-      {...this.props} />
-  }
-  componentDidMount(){
-    console.log('mounted')
+    let rows = this.state.data.map( person => {
+      return <PersonRow key={person.id} data={person} />
+    })
+    return <table>
+      <tbody>{rows}</tbody>
+    </table>
   }
 }
 
-const Button = (props) => <button onClick={props.update}>
-                            {props.txt} - {props.val}
-                          </button>
-
-const Label = (props) => <label onMouseMove={props.update}>
-                            {props.txt} - {props.val}
-                          </label>
-
-let ButtonMixed = Mixin(Button)
-let LabelMixed = Mixin(Label)
-
-class App extends React.Component {
-
-  render(){
-    return (
-      <div>
-        <ButtonMixed txt="Button" />
-        <LabelMixed txt="Label" />
-      </div>
-    );
-  }
-
+const PersonRow = (props) => {
+  return <tr>
+    <td>{props.data.id}</td>
+    <td> {props.data.name}</td>
+  </tr>
 }
-
-ReactDOM.render(<App />, document.getElementById('app'));
-
 
 export default App
