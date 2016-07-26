f63e938a80eb0795579a8e68228bdda0bf8886d1
Rafael Câmara Magalhães <rafaelcamaram@gmail.com>
2016-07-26 09:49:04 -0300

    Accessing Child Properties

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 //class App extends React.Component {
 //  render() {
@@ -29,36 +30,83 @@ import React from 'react';
 //    )
 //  }
 //}
-
+//
 //const App = () => <div>Teste2</div>
-
+//
 //var App = React.createClass({
 //  render: function() {
 //    return <div>Diferente</div>
 //  }
 //});
 
+//class App extends React.Component {
+//  
+//  constructor(){
+//    super();
+//    this.state = {
+//      red: 128,
+//      green: 128,
+//      blue: 128
+//    }
+//    this.update = this.update.bind(this)
+//  }
+//  
+//  update(e){
+//    this.setState(
+//			{	
+//      	red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+//      	green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+//      	blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+//    	}
+//		)
+//  }
+//  
+//  render(){
+//    return (
+//		
+//      <div>
+//        <Slider ref="red" update={this.update} />
+//        {this.state.red}
+//        <br />
+//        <Slider ref="green" update={this.update} />
+//        {this.state.green}
+//        <br />
+//        <Slider ref="blue" update={this.update} />
+//        {this.state.blue}
+//		
+//        <br />
+//      </div>
+//    );
+//  }
+//}
+//
+//class Slider extends React.Component {
+//  
+//  render(){
+//    return (
+//        <div>
+//          <input ref="inp" type="range" min="0" max="255" onChange={this.props.update} />
+//        </div>
+//    );
+//  }
+//}
+
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      txt: "Text Example",
-      cat: 0
-    }
-  }
-  
-  update(e) {
-    this.setState({txt: e.target.value})
-  }
-  
-  render() {
-    return (
-      <div>
-        <input type="text" onChange={this.update.bind(this)} />
-        <h1>{this.state.txt}</h1>
-      </div>
-    )
-  }
+	render() {
+//		return <Componente children="Rafael" /> The same
+		return <Componente>Rafael <Heart /> React </Componente>
+	}
 }
 
+class Componente extends React.Component {
+	render() {
+		 return <button>{this.props.children}</button>
+	}
+}
+
+class Heart extends React.Component {
+	render() {
+		return <span className="glyphicon glyphicon-heart" />
+	}
+}
 export default App
